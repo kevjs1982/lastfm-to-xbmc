8bfe58ee446bd176e4eb9c3b2f5f07cceae5ec66
Kev <git@kjs1982.me.uk>
2012-06-22 13:54:08 +0100

    Inital stub file

---
+++ last_fm_to_xbmc.php
@@ -0,0 +1,28 @@
+// Check for the various bits needed
+
+// Does the ini file exist?
+
+	// If yes read it and prompt for missing bit
+
+	// If no 
+		// Can I write it?
+		// prompt for username
+		// prompt for password
+		// Try and find the library, else prompt for it
+		
+// Does my library exist?
+
+	// If yes read it, then top up
+	
+	// If no create it
+	
+		// populate it with common aliases
+	
+		// populate it with Last FM data...
+		
+// grab each song in turn
+	// Does the musicbrainz value exist?  Use that
+	// Exact match
+	// Punctuation free match
+
+// Generate some statistics
